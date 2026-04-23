990777ab941f12254cabf20f07be0be43e308ba3
luhveestore-source <luhvee.store@gmail.com>
2026-04-23 17:29:36 -0300

    Excluir formatter.py.

--- formatter.py
+++
@@ -1,8 +0,0 @@
-def formatar_instagram(msg):
-    return msg + "\n\n#bomdia #motivação #luhveestore #promoções"
-
-def formatar_whatsapp(msg):
-    return msg  # mais simples
-
-def formatar_telegram(msg):
-    return "🔥 LuhVee Stores\n\n" + msg
